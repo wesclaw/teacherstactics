a6cf41081138cde0c9d3347f11a217bb15445682
wesclaw <108541397+wesclaw@users.noreply.github.com>
2024-02-29 16:45:48 +0100

    start over

--- index.php
+++ index.php
@@ -4,80 +4,14 @@
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>TeachersTactics</title>
+    <title>Teachers Tactics</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Caprasimo&family=Concert+One&family=Mouse+Memoirs&display=swap" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="">
   </head>
   <body>
- 
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container">
-    <a class="navbar-brand" href="#">
-      <div style="display: flex; justify-content: center; align-items: center;">
-        <img src="icons/education.png" class="logo img-fluid">
-        <div class="logo-title">
-          <h4>Teachers</h4>
-          <h4>Tactics</h4>
-        </div>
-      </div>
-    </a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      
-      <ul class="navbar-nav ms-auto">
-        
-        <li class="nav-item">
-          <a class="nav-link  underline-li" aria-current="page" href="#">Lesson Plans</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link underline-li" href="#">Games</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link underline-li" href="#">Arts & Crafts</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link underline-li">Songs</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link underline-li">Expirements</a>
-        </li>
-      
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Ages
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Preschool</a></li>
-            <li><a class="dropdown-item" href="#">Primary</a></li>
-            <li><a class="dropdown-item" href="#">Teens</a></li>
-            <li><a class="dropdown-item" href="#">Adults</a></li>
-          </ul>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
-
-<main>
-  <h1>The PHP Framework</h1>
-  <h2>for Preschool Teachers</h2>
-  <p>We focus on creating materials that can have an impact on your students. We’ve already laid the foundation — freeing you to create without sweating the small things.</p>
-  <div>
-  <button class="sign-in">Sign In</button>
-  <button class='sign-up'>Sign Up</button>
-  </div>
   
-</main>
-
-   
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    
     <script src="" async defer></script>
   </body>
 </html>
