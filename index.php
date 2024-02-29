4985f9a02c01dbed5527f56640d16359d7d9e61a
wesclaw <108541397+wesclaw@users.noreply.github.com>
2024-02-29 21:03:49 +0100

    navbar and main

--- index.php
+++ index.php
@@ -7,11 +7,71 @@
     <title>Teachers Tactics</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles.css">
   </head>
   <body>
-  
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container">
+      <a href="#"><img src="icons/" class="nav-logo"></a>
+
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav ms-auto">
+        
+        <li class="nav-item">
+          <img src="icons/lesson.png" class="nav-icon">
+          <a class="nav-link" href="#">Lesson Plans</a>
+        </li>
+        <li class="nav-item">
+          <img src="icons/pencil.png" class="nav-icon">
+          <a class="nav-link" href="#">Worksheets</a>
+        </li>
+        <li class="nav-item">
+        <img src="icons/games.png" class="nav-icon">
+          <a class="nav-link" href="#">Games</a>
+        </li>
+        <li class="nav-item">
+        <img src="icons/paint.png" class="nav-icon">
+          <a class="nav-link" aria-current="page" href="#">Arts & Crafts</a>
+        </li>
+        <li class="nav-item">
+        <img src="icons/note.png" class="nav-icon">
+          <a class="nav-link" href="#">Songs</a>
+        </li>
+        <li class="nav-item">
+        <img src="icons/science.png" class="nav-icon">
+          <a class="nav-link" href="#">Expirements</a>
+        </li>
+
+      </ul>
+    </div>
+  </div>
+</nav>
+<main>
+  <div class="container">
+    <h1>Innovate Your English Classroom</h1>
+    <p>We provide all the <strong>resources</strong> you need for your classroom and school. Let us do the planning <strong>while you focus on the teaching</strong>.</p>
+    <div> 
+    <button class='login-btn'>
+      <div style="display: flex; justify-content: center; align-items: center; gap: 10px;">
+      <img src="icons/login.png" class="btn-icon">
+      <p>Log In</p>
+      </div>
+    </button>
+    <button class='member-btn'>
+      <div style="display: flex; justify-content: center; align-items: center; gap: 10px;">
+      <img src="icons/plus.png" class="btn-icon">
+      <p>Sign Up</p>
+      </div>
+    </button>
+    </div>
     
+  </div>
+</main>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <script src="" async defer></script>
   </body>
 </html>
