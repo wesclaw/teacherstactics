e57f3070b19ca953377555b8d1c28a3d3fe3400f
wesclaw <108541397+wesclaw@users.noreply.github.com>
2024-03-01 07:02:52 +0100

    include file

--- index.php
+++ index.php
@@ -11,45 +11,7 @@
     <link rel="stylesheet" href="styles.css">
   </head>
   <body>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container">
-      <a href="#"><img src="icons/" class="nav-logo"></a>
-
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavDropdown">
-      <ul class="navbar-nav ms-auto">
-        
-        <li class="nav-item">
-          <img src="icons/lesson.png" class="nav-icon">
-          <a class="nav-link" href="#">Lesson Plans</a>
-        </li>
-        <li class="nav-item">
-          <img src="icons/pencil.png" class="nav-icon">
-          <a class="nav-link" href="#">Worksheets</a>
-        </li>
-        <li class="nav-item">
-        <img src="icons/games.png" class="nav-icon">
-          <a class="nav-link" href="#">Games</a>
-        </li>
-        <li class="nav-item">
-        <img src="icons/paint.png" class="nav-icon">
-          <a class="nav-link" aria-current="page" href="#">Arts & Crafts</a>
-        </li>
-        <li class="nav-item">
-        <img src="icons/note.png" class="nav-icon">
-          <a class="nav-link" href="#">Songs</a>
-        </li>
-        <li class="nav-item">
-        <img src="icons/science.png" class="nav-icon">
-          <a class="nav-link" href="#">Expirements</a>
-        </li>
-
-      </ul>
-    </div>
-  </div>
-</nav>
+    <?php include("includes/navbar.html")?>
 <main>
   <div class="container">
     <h1>Innovate Your English Classroom</h1>
@@ -68,7 +30,6 @@
       </div>
     </button>
     </div>
-    
   </div>
 </main>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
