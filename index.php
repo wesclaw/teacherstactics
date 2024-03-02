df1eeb4b64ba723496337f1a2a6db45fdf08c180
wesclaw <108541397+wesclaw@users.noreply.github.com>
2024-03-02 11:22:24 +0100

    main section

--- index.php
+++ index.php
@@ -4,9 +4,10 @@
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Teachers Tactics</title>
+    <title>TeachersTactics</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="icons/logo-pencil.png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="styles.css">
   </head>
@@ -15,7 +16,7 @@
 <main>
   <div class="container">
     <h1>Innovate Your English Classroom</h1>
-    <p>We provide all the <strong>resources</strong> you need for your classroom and school. Let us do the planning <strong>while you focus on the teaching</strong>.</p>
+    <p>We provide all the <strong>resources</strong> you need for your classroom and school. Let us do the planning <strong>while you focus on the teaching.</strong></p>
     <div> 
     <button class='login-btn'>
       <div style="display: flex; justify-content: center; align-items: center; gap: 10px;">
@@ -32,6 +33,11 @@
     </div>
   </div>
 </main>
+<section class='taglines'>
+  <div class="container">
+  
+  </div>
+</section>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <script src="" async defer></script>
   </body>
