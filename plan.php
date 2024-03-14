519edcc7a58c696a54c0efacc2edd89b83f2ee0d
wesclaw <108541397+wesclaw@users.noreply.github.com>
2024-03-14 20:16:51 +0100

    update

--- plan.php
+++ plan.php
@@ -4,7 +4,7 @@
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>//use a foreach/foreach plan with e listener, i encodeauricomponet based on the database table names, then display that data</title>
+    <title></title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
@@ -15,7 +15,15 @@
     <!-- This page will fetch from the database and display the plan based on the users click -->
 
     <div class="container">
-      <div class="plan"></div>
+    <div class="make-this-sticky">
+    <button id="go_back_btn" class="back-btn"><img src="icons/back.png" alt="back btn">Back to plans</button>
+  </div>
+      <div class="sidebar">
+
+      </div>
+      <div class="plan">
+          <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere vero, libero tenetur illo voluptate, est incidunt esse, eaque dolore mollitia voluptatem aut exercitationem a alias! Alias, delectus? Veritatis necessitatibus animi fugiat id! Sapiente eaque cum, dolorum officiis placeat nostrum exercitationem enim! Sequi assumenda minus unde, labore nihil sit? At illum debitis consectetur in vel! Vero tempore magni molestias explicabo ipsa tempora non enim exercitationem tenetur doloremque eaque, eveniet adipisci fugiat optio saepe quasi aspernatur neque provident id quis incidunt labore eos deserunt? Itaque quibusdam doloremque non eligendi qui maxime vitae beatae similique dignissimos quas voluptates, nulla amet sint? Minus, repudiandae.</h1>
+      </div>
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
