248fa134f47057c0d0a03b98b3e58f9c86b2bfb9
wesclaw <108541397+wesclaw@users.noreply.github.com>
2024-05-27 20:50:38 +0200

    update

--- games.php
+++ games.php
@@ -13,6 +13,10 @@
   </head>
   <body>
   <?php include("includes/navbar.html") ?>
+
+    <div class="container main-section">
+    <h1 class="coming_soon_text">Coming Soon!</h1>
+    </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <script src="" async defer></script>
   </body>
